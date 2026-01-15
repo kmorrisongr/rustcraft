fd65e818f244fa562bc0a4799c6c8a2f7726494d
kmorrisongr <mckinleybagel@pm.me>
2026-01-15 00:18:02 -0500

    easily comment/uncomment constants for development

--- client/src/constants.rs
+++ client/src/constants.rs
@@ -3,7 +3,9 @@ pub const BASE_ROUGHNESS: f32 = 0.6;
 pub const BASE_SPECULAR_HIGHLIGHT: f32 = 0.;
 
 // 20 ticks * 60 seconds * 20 minutes
-pub const DAY_DURATION_IN_TICKS: u64 = 20 * 60 * 20;
+// pub const DAY_DURATION_IN_TICKS: u64 = 20 * 60 * 20;
+// Debug/testing: 1 minute
+pub const DAY_DURATION_IN_TICKS: u64 = 20 * 60;
 
 pub const MAX_HOTBAR_SLOTS: u32 = 9;
 
